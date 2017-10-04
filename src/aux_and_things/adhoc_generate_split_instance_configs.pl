fc1a03e1a8a6a8f2218684a3289e74e7c22c297a
Adam Liska <adliska@fb.com>
2017-10-04 00:49:58 -0700

    model checkpointing in training and testing
    Summary:
    Model checkpointing in training and testing. One single model is trained on atomic
    and composed tasks and then loaded separately in the compostional & control tesk setups.
    
    Differential Revision: D5966726
    
    fbshipit-source-id: 7e5091c84447cb96bd69529127d4fccf456238da

--- src/aux_and_things/adhoc_generate_split_instance_configs.pl
+++ src/aux_and_things/adhoc_generate_split_instance_configs.pl
@@ -1,23 +1,19 @@
 #!/usr/bin/perl -w
 
 $name_string = shift;
-
 $num_bits = shift;
-
 $composition_type = shift;
-
 $lookup_table_name = shift;
-
 $atomic_task_count = shift;
-
 $atomic_task_episodes_count = shift;
-
 $composed_task_train_episodes_count = shift;
-
 $composed_task_test_episodes_count = shift;
+# Configuration file type: TRAIN_SEPARATE or ALL_TOGETHER
+$config_type = shift;
 
-$comp_file = "tasks_config." . $name_string . "_compositional.json";
-$control_file = "tasks_config." . $name_string . "_control.json";
+# Output file names for the compositional and control setups
+$comp_file = $name_string . "_compositional.json";
+$control_file = $name_string . "_control.json";
 
 `rm -f $comp_file`;
 `rm -f $control_file`;
@@ -25,6 +21,13 @@ $control_file = "tasks_config." . $name_string . "_control.json";
 open COMP,">$comp_file";
 open CONTROL,">$control_file";
 
+if ($config_type eq "TRAIN_SEPARATE") {
+ $train_file = $name_string . "_train.json";
+ `rm -f $train_file`;
+ open TRAIN,">$train_file";
+}
+
+# 1. PRINT THE PREAMBLE
 $preamble = << 'END_PREAMBLE';
 {
   "worlds": {
@@ -36,10 +39,13 @@ $preamble = << 'END_PREAMBLE';
   {
 END_PREAMBLE
 
+if ($config_type eq "TRAIN_SEPARATE") {
+  print TRAIN $preamble;
+}
 print COMP $preamble;
 print CONTROL $preamble;
 
-
+# 2. PRINT THE SUPERSET OF TASKS USED IN THIS CONFIG
 @combinations = ();
 
 $i=1;
@@ -49,7 +55,9 @@ while ($i<=$atomic_task_count) {
       "type": "tasks.micro.$lookup_table_name.LookupTaskR${num_bits}D$i"
     },
 END_TASK_SPECIFICATION
-
+    if ($config_type eq "TRAIN_SEPARATE") {
+      print TRAIN $task_specification;
+    }
     print COMP $task_specification;
     print CONTROL $task_specification;
 
@@ -74,6 +82,9 @@ END_TASK_SPECIFICATION
         if ($i==$atomic_task_count && $j==$atomic_task_count) {
             $composed_task_specification =~ s/,$//;
         }
+        if ($config_type eq "TRAIN_SEPARATE") {
+          print TRAIN $composed_task_specification;
+        }
         print COMP $composed_task_specification;
         print CONTROL $composed_task_specification;
         push @combinations, $i . "_" . $j;
@@ -82,6 +93,7 @@ END_TASK_SPECIFICATION
     $i++;
 }
 
+# 3. PRINT THE SCHEDULER
 $connector = << 'CONNECTOR';
   },
   "scheduler":
@@ -91,75 +103,181 @@ $connector = << 'CONNECTOR';
          "tasks": [
 CONNECTOR
 chomp $connector;
+if ($config_type eq "TRAIN_SEPARATE") {
+  print TRAIN $connector;
+}
 print COMP $connector;
 print CONTROL $connector;
 
-foreach $i (1..$atomic_task_count) {
-    print COMP "\"LookupTaskR${num_bits}D$i\", ";
-    print CONTROL "\"LookupTaskR${num_bits}D$i\", ";
+# 3.1 Print tasks
+if ($atomic_task_episodes_count > 0) {
+  @atomic_names = ();
+  foreach $i (1..$atomic_task_count) {
+    $atomic_name = "\"LookupTaskR${num_bits}D$i\"";
+    push @atomic_names, $atomic_name;
+  }
+  if ($config_type eq "TRAIN_SEPARATE") {
+    print TRAIN join ", ", @atomic_names;
+  } else {
+    print COMP join ", ", @atomic_names;
+    print CONTROL join ", ", @atomic_names;
+  }
 }
 
-@composed_names = ();
-foreach $i_j (@combinations) {
-    $composed_name = "\"" . $composition_type . "LookupTaskR${num_bits}D" . $i_j . "\"";
-    push @composed_names,$composed_name;
-}
-print COMP join ", ", @composed_names;
-print CONTROL join ", ", @composed_names;
-
-print COMP ", ";
-print CONTROL ", ";
-
-
-@composed_test_names = ();
-@control_test_names = ();
-foreach $i_j (@combinations) {
-    $composed_test_name =  "\"" . $composition_type . "LookupTestTaskR${num_bits}D" . $i_j . "\"";
-    ($i,$j) = split "_",$i_j;
-    push @composed_test_names,$composed_test_name;
-    $control_test_name = "\"" . $composition_type . "LookupTestTaskR${num_bits}D" . ($i+$atomic_task_count) . "_" .
-        ($j+$atomic_task_count) . "\"";
-    push @control_test_names,$control_test_name;
-}
-print COMP join ", ", @composed_test_names;
-print CONTROL join ", ", @control_test_names;
-
-print COMP "\],\n         \"intervals\": \[";
-print CONTROL "\],\n         \"intervals\": \[";
-
-$begin = 0;
-$end = $atomic_task_episodes_count-1;
-foreach $i (1..$atomic_task_count) {
-    print COMP "\[" . $begin . "," . $end . "\], ";
-    print CONTROL "\[" . $begin . "," . $end . "\], ";
+if ($composed_task_train_episodes_count > 0) {
+  if ($atomic_task_episodes_count > 0) {
+    if ($config_type eq "TRAIN_SEPARATE") {
+      print TRAIN ", ";
+    } else {
+      print COMP ", ";
+      print CONTROL ", ";
+    }
+  }
+  @composed_names = ();
+  foreach $i_j (@combinations) {
+      $composed_name = "\"" . $composition_type . "LookupTaskR${num_bits}D"
+        . $i_j . "\"";
+      push @composed_names,$composed_name;
+  }
+  if ($config_type eq "TRAIN_SEPARATE") {
+    print TRAIN join ", ", @composed_names;
+  } else {
+    print COMP join ", ", @composed_names;
+    print CONTROL join ", ", @composed_names;
+  }
 }
 
-
-$begin = $end+1;
-$end += $composed_task_train_episodes_count;
-
-foreach $i (1..($atomic_task_count**2)) {
-    print COMP "\[" . $begin . "," . $end . "\], ";
-    print CONTROL "\[" . $begin . "," . $end . "\], ";
+if ($composed_task_test_episodes_count > 0) {
+  if (($composed_task_train_episodes_count > 0 ||
+    $atomic_task_episodes_count > 0) && $config_type ne "TRAIN_SEPARATE") {
+    print COMP ", ";
+    print CONTROL ", ";
+  }
+  @composed_test_names = ();
+  @control_test_names = ();
+  foreach $i_j (@combinations) {
+      $composed_test_name =  "\"" . $composition_type
+        . "LookupTestTaskR${num_bits}D" . $i_j . "\"";
+      ($i,$j) = split "_",$i_j;
+      push @composed_test_names,$composed_test_name;
+      $control_test_name = "\"" . $composition_type
+        . "LookupTestTaskR${num_bits}D" . ($i+$atomic_task_count)
+        . "_" . ($j+$atomic_task_count) . "\"";
+      push @control_test_names,$control_test_name;
+  }
+  print COMP join ", ", @composed_test_names;
+  print CONTROL join ", ", @control_test_names;
 }
 
-$begin = $end+1;
-$end += $composed_task_test_episodes_count;
-
-foreach $i (1..($atomic_task_count**2-1)) {
-    print COMP "\[" . $begin . "," . $end . "\], ";
-    print CONTROL "\[" . $begin . "," . $end . "\], ";
+if ($config_type eq "TRAIN_SEPARATE") {
+  print TRAIN "\],\n";
 }
-print COMP "\[" . $begin . "," . $end . "\]\]\n";
-print CONTROL "\[" . $begin . "," . $end . "\]\]\n";
+print COMP "\],\n";
+print CONTROL "\],\n";
 
+# 3.2 Print intervals
+if ($config_type eq "TRAIN_SEPARATE") {
+  print TRAIN "         \"intervals\": \[";
+}
+print COMP "         \"intervals\": \[";
+print CONTROL "         \"intervals\": \[";
+
+
+if ($atomic_task_episodes_count > 0) {
+  $begin = 0;
+  $end = $atomic_task_episodes_count-1;
+
+  foreach $i (1..$atomic_task_count) {
+      if ($config_type eq "TRAIN_SEPARATE") {
+        print TRAIN "\[" . $begin . "," . $end . "\]";
+      } else {
+        print COMP "\[" . $begin . "," . $end . "\]";
+        print CONTROL "\[" . $begin . "," . $end . "\]";
+      }
+      if ($i < $atomic_task_count) {
+        if ($config_type eq "TRAIN_SEPARATE") {
+          print TRAIN ", ";
+        } else {
+          print COMP ", ";
+          print CONTROL ", ";
+        }
+      }
+  }
+}
+
+if ($composed_task_train_episodes_count > 0) {
+  if ($atomic_task_episodes_count > 0) {
+    if ($config_type eq "TRAIN_SEPARATE") {
+      print TRAIN ", ";
+    } else {
+      print COMP ", ";
+      print CONTROL ", ";
+    }
+  }
+
+  $begin = $atomic_task_episodes_count;
+  $end = $begin + $composed_task_train_episodes_count - 1;
+
+  foreach $i (1..($atomic_task_count**2)) {
+    if ($config_type eq "TRAIN_SEPARATE") {
+      print TRAIN "\[" . $begin . "," . $end . "\]";
+      if ($i < $atomic_task_count**2) {
+        print TRAIN ", ";
+      }
+    } else {
+      print COMP "\[" . $begin . "," . $end . "\]";
+      print CONTROL "\[" . $begin . "," . $end . "\]";
+      if ($i < $atomic_task_count**2) {
+        print COMP ", ";
+        print CONTROL ", ";
+      }
+    }
+  }
+}
+
+if ($composed_task_test_episodes_count > 0) {
+  if ($config_type eq "TRAIN_SEPARATE") {
+    $begin = 0;
+  } else {
+    $begin = $atomic_task_episodes_count + $composed_task_train_episodes_count;
+  }
+  $end = $begin + $composed_task_test_episodes_count - 1;
+
+  if (($atomic_task_episodes_count > 0 ||
+    $composed_task_train_episodes_count > 0) &&
+    $config_type ne "TRAIN_SEPARATE") {
+      print COMP ", ";
+      print CONTROL ", ";
+  }
+
+  foreach $i (1..($atomic_task_count**2-1)) {
+      print COMP "\[" . $begin . "," . $end . "\], ";
+      print CONTROL "\[" . $begin . "," . $end . "\], ";
+  }
+  print COMP "\[" . $begin . "," . $end . "\]";
+  print CONTROL "\[" . $begin . "," . $end . "\]";
+}
+
+if ($config_type eq "TRAIN_SEPARATE") {
+  print TRAIN "\]\n";
+}
+print COMP "\]\n";
+print CONTROL "\]\n";
+
+# 4. CLOSE OFF THE TASK CONFIG FILE
 $bottom = << 'END_BOTTOM';
      }
   }
 }
 END_BOTTOM
+if ($config_type eq "TRAIN_SEPARATE") {
+  print TRAIN $bottom;
+}
 print COMP $bottom;
 print CONTROL $bottom;
 
+if ($config_type eq "TRAIN_SEPARATE") {
+  close TRAIN;
+}
 close COMP;
 close CONTROL;
